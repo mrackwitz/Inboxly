09ed8cfd3768ae2b2cb395d0ece12f2a4289a28e
Marius Rackwitz <git@mariusrackwitz.de>
2016-05-23 12:49:40 +0200

    Implement CacheController

--- Inboxly/CacheController.swift
+++ Inboxly/CacheController.swift
@@ -4,20 +4,40 @@ import RealmSwift
 //
 // TODOs:
 //
-// * [ ] Observe for new photos
-// * [ ] Load the photo from the bundle
+// * [x] Observe for new photos
+// * [x] Load the photo from the bundle
 //       (instead of an actual download)
-// * [ ] Write the data to a `Photo` model
+// * [x] Write the data to a `Photo` model
 //
 
 class CacheController {
+
+    lazy var realm = try! Realm()
+
+    lazy var pendingPhotos: Results<Photo> = {
+        self.realm.objects(Photo).filter("image = nil")
+    }()
+
+    var subscription: NotificationToken?
+
     init() {
+        subscription = pendingPhotos.addNotificationBlock { [weak self] _ in
+            self?.downloadPhotos()
+        }
     }
 
     func downloadPhotos() {
-        //<#Needs to be implemented.#>
+        try! realm.write {
+            for pending in pendingPhotos {
+                print("[Cache] Download \(pending.url)")
+
+                let image = UIImage(named: pending.url)! // faux download
+                pending.image = UIImagePNGRepresentation(image)!
+            }
+        }
     }
     
     deinit {
+        subscription?.stop()
     }
 }
